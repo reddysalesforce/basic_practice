48a4b15241dbd18616a1174b52f48e5c0da232c1
reddysalesforce <reddysfcc@gmail.com>
2023-07-05 21:35:05 +0530

    passing query parameter

--- sfra_training/cartridge/controllers/Hello.js
+++ sfra_training/cartridge/controllers/Hello.js
@@ -3,9 +3,10 @@
 var server = require('server');
 var cache = require('*/cartridge/scripts/middleware/cache');
 
-server.get('Show',cache.applyDefaultCache,function(rq,res,next){
+server.get('Show',cache.applyDefaultCache,function(req,res,next){
     var Site = require('dw/system/Site');
-    res.render('helloWorld',{param1: Site.current.name});
+    var param2Data = req.querystring.param2;
+    res.render('helloWorld',{param1: Site.current.name, param2: param2Data});
     next();
 });
 
