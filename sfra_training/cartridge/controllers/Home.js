d68581dc678375ec1f9bc1a0d3049ac43d6d6206
reddysalesforce <reddysfcc@gmail.com>
2023-07-05 23:00:34 +0530

    append, replace added

--- sfra_training/cartridge/controllers/Home.js
+++ sfra_training/cartridge/controllers/Home.js
@@ -18,5 +18,40 @@ server.prepend('Show', cache.applyDefaultCache, function (req, res, next) {
     next();
 });
 
+server.append('Show', cache.applyDefaultCache, function(req, res, next){
+    var viewData = res.getViewData();
+    var appendParam = "This is param from append";
+    var queryparam = req.querystring.param ? req.querystring.param : "no paramter was passed";
+    res.setViewData({
+        param1 : viewData.param1 + ' AND ' + appendParam + ' AND querystring param = '+ queryparam
+    });
+    next();
+})
+
+// server.replace('Show', consentTracking.consent, cache.applyDefaultCache, function (req, res, next) {
+//     var Site = require('dw/system/Site');
+//     var PageMgr = require('dw/experience/PageMgr');
+//     var pageMetaHelper = require('*/cartridge/scripts/helpers/pageMetaHelper');
+
+//     pageMetaHelper.setPageMetaTags(req.pageMetaData, Site.current);
+
+//     var page = PageMgr.getPage('homepage');
+
+//     if (page && page.isVisible()) {
+//         res.page('homepage');
+//     } else {
+//         res.render('home/homePage');
+//     }
+//     var viewData = res.getViewData();
+//     viewData.param1 = "this is from prepend";
+//     var replaceParam = "This is param from replace";
+//     var appendParam = "this is from append param";
+//     res.setViewData({
+//         param1: viewData.param1 + ' AND ' +  appendParam + ' AND ' + replaceParam
+//     });
+//     next();
+// }, pageMetaData.computedPageMetaData);
+
+
 
 module.exports = server.exports();
