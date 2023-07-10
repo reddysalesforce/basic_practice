af654959ecad371b412f642c3aab480dd77eb4da
reddysalesforce <reddysfcc@gmail.com>
2023-07-10 18:45:15 +0530

    extending model function - availability.js

--- sfra_training/cartridge/models/product/decorators/availability.js
+++ sfra_training/cartridge/models/product/decorators/availability.js
@@ -1,34 +1,34 @@
-'use strict';
+    'use strict';
 
-var resource = require('dw/web/Resource');
-var base = module.superModule;
+    var Resource = require('dw/web/Resource');
+    var base = module.superModule;
 
-module.exports = function (object, quantity, minOrderQuantity, availabilityModel) {
-    //invoke the availability model on the base
-    base.call(this, object, quantity, minOrderQuantity, availabilityModel);
-    
-    //Define a new property in the model with ATS as its value
-    Object.defineProperty(object, 'ats', {
-        enumerable: true,
-        value: getATSMessage(availabilityModel)
-    });
-};
+    module.exports = function (object, quantity, minOrderQuantity, availabilityModel) {
+        //invoke the availability model on the base
+        base.call(this, object, quantity, minOrderQuantity, availabilityModel);
+        
+        //Define a new property in the model with ATS as its value
+        Object.defineProperty(object, 'ats', {
+            enumerable: true,
+            value: getATSMessage(availabilityModel)
+        });
+    };
 
-function getATSMessage(availabilityModel){
-    var ATS = {};
-    ATS.messages = [];
-    var inventoryRecord = availabilityModel.inventoryRecord;
-    
-    //Add a new message to the array of avaialbility messages(just like the base does)
-    if(inventoryRecord) {
-        ATS.messages.push(
-            Resource.msgf(
-                'label.quantity.in.stock',
-                'common',
-                null,
-                inventoryRecord.ATS.value
-            )
-        );
+    function getATSMessage(availabilityModel){
+        var ATS = {};
+        ATS.messages = [];
+        var inventoryRecord = availabilityModel.inventoryRecord;
+        
+        //Add a new message to the array of avaialbility messages(just like the base does)
+        if(inventoryRecord) {
+            ATS.messages.push(
+                Resource.msgf(
+                    'label.quantity.in.stock',
+                    'common',
+                    null,
+                    inventoryRecord.ATS.value
+                )
+            );
+        }
+        return ATS;
     }
-    return ATS;
-}
