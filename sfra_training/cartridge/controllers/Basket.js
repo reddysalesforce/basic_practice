c5361e2fa5f448a3fe416f8d396fd3886705100a
reddysalesforce <reddysfcc@gmail.com>
2023-07-11 18:04:59 +0530

    representing basket data as model

--- sfra_training/cartridge/controllers/Basket.js
+++ sfra_training/cartridge/controllers/Basket.js
@@ -5,7 +5,9 @@ var server = require('server');
 server.get('Show',function(req, res, next){
     var BasketMgr = require('dw/order/BasketMgr');
     var currentBasket = BasketMgr.getCurrentBasket();
-    res.render('basket',{basket:currentBasket});
+    var CartModel = require('*/cartridge/models/cart');
+    var basketModel = new CartModel(BasketMgr.currentBasket);
+    res.render('basket',{basketModel:basketModel});
     next();
 });
 
