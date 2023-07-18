14f46b0621bffc5548e6cae90961c231d4ccb6bd
reddysalesforce <reddysfcc@gmail.com>
2023-07-19 00:52:09 +0530

    Use Client Side JavaScript to Validate and Submit the Form

--- sfra_training/cartridges/sfra_training/cartridge/controllers/Newsletter.js
+++ sfra_training/cartridges/sfra_training/cartridge/controllers/Newsletter.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var server = require('server');
+var URLUtils = require('dw/web/URLUtils');
 
 server.get('Show', server.middleware.https, function (req, res, next) {
   var actionUrl = dw.web.URLUtils.url('Newsletter-Handler');
@@ -22,22 +23,36 @@ server.post(
         var newsletterForm = server.forms.getForm('newsletter');
         var continueUrl = dw.web.URLUtils.url('Newsletter-Show');
 
-        // Perform any server-side validation before this point, and invalidate form accordingly
         if (newsletterForm.valid) {
-            // Send back a success status, and a redirect to another route
-            res.render('/newsletter/newslettersuccess', {
-                continueUrl: continueUrl,
-                newsletterForm: newsletterForm
+            // Show the success page
+            res.json({
+                success: true,
+                redirectUrl: URLUtils.url('Newsletter-Success').toString()
             });
         } else {
             // Handle server-side validation errors here: this is just an example
-            res.render('/newsletter/newslettererror', {
-                errorMsg: dw.web.Resource.msg('error.crossfieldvalidation', 'newsletter', null),
-                continueUrl: continueUrl
+            res.setStatusCode(500);
+            res.json({
+                error: true,
+                redirectUrl: URLUtils.url('Error-Start').toString()
             });
         }
 
         next();
     }
 );
+
+server.get(
+    'Success',
+    server.middleware.https,
+    function (req, res, next) {
+        res.render('/newsletter/newslettersuccess', {
+            continueUrl: URLUtils.url('Newsletter-Show'),
+            newsletterForm: server.forms.getForm('newsletter')
+        });
+
+        next();
+    }
+);
+
 module.exports = server.exports();
